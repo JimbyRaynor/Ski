06bb0c302e0c999ccc72bd29a78bb41d0d9185e2
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2024-06-25 09:52:00 +1000

    Add files via upload

--- ski.py
+++ ski.py
@@ -13,31 +13,67 @@ canvas1.place(x=0,y=0)
 
 # player class (template)
 
-class Player:
-    def __init__(self,myfilename="",x=0,y=0):
+class GameObject:
+    def __init__(self,filedown="",fileleft="",fileright="",x=0,y=0):
         self.x = x
         self.y = y
-        self.image = PhotoImage(file=myfilename).zoom(4,4)
+        self.image = PhotoImage(file=filedown).zoom(2,2)
         self.sprite = canvas1.create_image(2,2,anchor=NW,image=self.image)
+        if fileleft != "":
+           self.imageleft = PhotoImage(file=fileleft).zoom(2,2)
+        if fileright != "":
+           self.imageright = PhotoImage(file=fileright).zoom(2,2)
         canvas1.move(self.sprite,x,y)
     def move(self,dx=0,dy=0):
         self.x = self.x + dx
         self.y = self.y + dy
         canvas1.move(self.sprite,dx,dy)
+    def faceleft(self):
+        canvas1.itemconfigure(self.sprite,image=self.imageleft)
+    def faceright(self):
+        canvas1.itemconfigure(self.sprite,image=self.imageright)
+    def facedown(self):
+        canvas1.itemconfigure(self.sprite,image=self.image)
         
 
+player1 = GameObject("skier.png","skier2.png","skier3.png",x=390,y=200)
 
+mountain = []
+mdx = 0 # move mountain amount
+mdy = -2
 
+for i in range(1000):
+    mountain.append(GameObject("flag1.png",\
+      x=random.randint(1,800),y=400+random.randint(1,20000)))
 
-player1 = Player("skier.png",200,200)
-
-flag1 = Player("flag1.png",400,400)
 
 
 def timerupdate():
     #player1.move(0,0)
-    flag1.move(0,-2)
-    mainwin.after(100,timerupdate)
+    for m in mountain:
+        m.move(mdx,mdy)
+    mainwin.after(50,timerupdate)
 
+def mykey(event):
+    global mdx, mdy
+    if event.char == "w":
+        mdx = 0
+        mdy = -2
+        player1.facedown()
+    if event.char == "d":
+        mdx = -2
+        mdy = -2
+        player1.faceright()
+    if event.char == "a":
+        mdx = 2
+        mdy = -2
+        player1.faceleft()
+    if event.char == "s":
+        mdx = 0
+        mdy = -4
+        player1.facedown()
+
+mainwin.bind("<Key>", mykey)
 mainwin.after(100,timerupdate)
 mainwin.mainloop()
+
