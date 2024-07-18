75ea8e8c4a419efdbc5d86f61324ef5f6a5bbfa4
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2024-07-18 15:30:15 +1000

    Add files via upload

--- ski2.py
+++ ski2.py
@@ -2,6 +2,7 @@ from tkinter import *
 import random
 import os
 import time
+import math
 
 mainwin = Tk(className=" Ski")
 
@@ -9,6 +10,10 @@ mainwin.geometry("800x680")
 # playground
 canvas1= Canvas(mainwin,width=800,height=600, bg = "white")
 canvas1.place(x=0,y=0)
+
+speed = 1
+theta = 3*math.pi/2
+
 
 # player class (template)
 class GameObject:
@@ -58,21 +63,19 @@ def adddirt(xloc=400,yloc=400):
     #icecircle.showcollisioncircles() # for debugging collisions
     mountainice.extend([icecircle])
 
-
-for i in range(40):
-    for j in range(200):
+# add dirt
+for i in range(24):
+    for j in range(40):
         if random.randint(1,10) == 1:
-          adddirt(i*50,j*50)
+          adddirt(i*50+20,j*50+400)
              
 player1 = GameObject("skier.png","skier2.png","skier3.png",x=390,y=200)
 player1.collisioncirclelist.append((10,2,0))
-player1.showcollisioncircles()    # for debugging collisions
+#player1.showcollisioncircles()    # for debugging collisions
         
 
 # create mountain obstacles
 mountain = []
-mdx = 0 # move mountain amount
-mdy = -2
 
 def addflag(xloc=400,yloc=400):
     img = GameObject("flag1.png",x=xloc,y=yloc)
@@ -86,13 +89,22 @@ def addtree(xloc=400,yloc=400):
     #img.showcollisioncircles()  # for debugging collisions
     mountain.append(img)
 
+def addwalls():
+    for j in range(80):
+        addtree(10,j*35+400)  # left
+        addtree(1200,j*35+400) # right
+        addtree(j*35,3200) # bottom
+
+
+addwalls()
+# add trees and flags
 for i in range(40):
-    for j in range(200):
+    for j in range(80):
         r = random.randint(1,20)
         if r == 1:
-            addflag(i*30,j*30)
+            addflag(i*30+20,j*30+400)
         if r == 2:
-            addtree(i*30,j*30)  
+            addtree(i*30+20,j*30+400)  
     
 def checkcollisioncircles(object1, object2):
     for c1 in object1.collisioncirclelist:
@@ -102,36 +114,54 @@ def checkcollisioncircles(object1, object2):
              < (c1[0]+c2[0])**2:
                return True
     return False
-    
+
+def sign(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
 def timerupdate():
+    global speed
+    speed = speed + 0.03
+    if speed >= 8: speed = 8
+    mdy = speed*math.sin(theta)
+    mdx = -speed*math.cos(theta)
     for m in mountain:
         m.move(mdx,mdy)
         if checkcollisioncircles(m,player1):
-            print("Circles Crash ;)");
+            print("Circles Crash "+str(speed));
+            speed = 1
     for m in mountainice:
-        m.move(mdx,mdy)    
-    mainwin.after(50,timerupdate)
+        m.move(mdx,mdy)
+    mainwin.after(20,timerupdate)
+
+def sign(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
 
 def mykey(event):
-    global mdx, mdy
+    global theta
     if event.char == "w":
-        mdx = 0
-        mdy = 0
+        theta = math.pi/2
         player1.facedown()
     if event.char == "d":
-        mdx = -2
-        mdy = -2
+        theta = -math.pi/4
         player1.faceright()
     if event.char == "a":
-        mdx = 2
-        mdy = -2
+        theta = 5*math.pi/4
         player1.faceleft()
     if event.char == "s":
-        mdx = 0
-        mdy = -4
+        theta = 3*math.pi/2
         player1.facedown()
 
 mainwin.bind("<Key>", mykey)
-mainwin.after(100,timerupdate)
+timerupdate()
 mainwin.mainloop()
 
